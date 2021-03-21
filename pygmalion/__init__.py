0dfa9a6b7166bca8427b632f31e695a0a0055f04
BenoitFavier <bade@webmails.top>
2021-03-21 22:16:40 +0100

    import all attributes of the utilities module

--- pygmalion/__init__.py
+++ pygmalion/__init__.py
@@ -1,5 +1,4 @@
 from . import neural_networks
 from . import datasets
-from .utilities import split, kfold, MSE, RMSE, R2, accuracy, plot_correlation
-from .utilities import plot_confusion_matrix, GPU_info, plot_bounding_boxes
+from .utilities import *
 from .info import __version__, __author__
