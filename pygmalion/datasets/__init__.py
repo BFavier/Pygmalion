68bce136c34e76ebb8d54af8876ed1ee4c393dea
BenoitFavier <bade@webmails.top>
2021-03-22 10:12:29 +0100

    typo in the name of the module

--- pygmalion/datasets/__init__.py
+++ pygmalion/datasets/__init__.py
@@ -1,6 +1,6 @@
 from ._aquarium import aquarium
 from ._boston_housing import boston_housing
-from .cityscapes import cityscapes
+from ._cityscapes import cityscapes
 from ._fashion_mnist import fashion_mnist
 from ._iris import iris
 from ._savannah import savannah
