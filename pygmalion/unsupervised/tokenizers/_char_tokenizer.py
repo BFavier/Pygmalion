c4abc03e001eca6a54e5b2fde611fcdca9138528
BenoitFavier <bade@webmails.top>
2021-08-04 16:40:35 +0200

    char tokenizer has been modified to encode into utf-8 bytes

--- pygmalion/unsupervised/tokenizers/_char_tokenizer.py
+++ pygmalion/unsupervised/tokenizers/_char_tokenizer.py
@@ -1,65 +1,40 @@
-from itertools import chain
-from collections import Counter
-from typing import Iterable, List, Dict
-from ._tokenizer import Tokenizer, SpecialToken
+from typing import List
+from ._tokenizer import Tokenizer
 
 
 class CharTokenizer(Tokenizer):
     """
-    Tokenizer that split text into single characters
+    Tokenizer that split text into single bytes chunks
+    (using UTF-8 encoding)
     """
-
-    _unknown = SpecialToken("UNKNOWN")
 
     @classmethod
     def from_dump(cls, dump: dict) -> "CharTokenizer":
         assert dump["type"] == cls.__name__
-        vocabulary = [cls._unknown]+dump["vocabulary"]
-        return CharTokenizer(vocabulary=vocabulary)
+        return CharTokenizer()
 
     def __repr__(self):
-        return f"{type(self).__name__}({len(self.vocabulary)} chars)"
+        return f"{type(self).__name__}()"
 
-    def __init__(self, vocabulary: List[str] = []):
-        """build a tokenizer from the given vocabulary"""
-        if self._unknown not in vocabulary:
-            vocabulary += [self._unknown]
-        self.vocabulary = vocabulary
-
-    def train(self, corpus: Iterable[str]) -> Dict[str, int]:
-        """
-        find all unique words from a corpus of whitespace separated sentences
-        """
-        char_count = Counter(chain(*corpus))
-        char_count = dict(sorted(char_count.items(),
-                                 key=lambda item: item[1],
-                                 reverse=True))
-        self.vocabulary = [self._unknown] + list(char_count.keys())
-        return char_count
+    def __init__(self):
+        pass
 
     def encode(self, sentence: str, regularize: bool = False) -> List[int]:
         """encode a sentence"""
-        return [self._char_indexes.get(c, 0) for c in sentence]
+        return list(sentence.encode("utf-8"))
 
     def decode(self, sentence: List[int]) -> str:
         """decode a sentence"""
-        return " ".join([str(self.vocabulary[i]) for i in sentence
-                         if i < self.n_tokens])
+        return bytes(sentence).decode("utf-8", errors="ignore")
 
     @property
     def vocabulary(self):
-        return self._vocabulary
-
-    @vocabulary.setter
-    def vocabulary(self, other):
-        self._vocabulary = other
-        self._char_indexes = {w: i for i, w in enumerate(self.vocabulary)}
+        return [bytes([i]) for i in range(256)]
 
     @property
     def n_tokens(self):
-        return len(self.vocabulary)
+        return 256
 
     @property
     def dump(self):
-        return {"type": type(self).__name__,
-                "vocabulary": self.vocabulary[1:]}
+        return {"type": type(self).__name__}
