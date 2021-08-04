33bb4f3dab0203e4188960642f741882fb9f360f
BenoitFavier <bade@webmails.top>
2021-08-04 17:44:56 +0200

    implemented linear complexity attention

--- pygmalion/neural_networks/layers/_multi_head_attention.py
+++ pygmalion/neural_networks/layers/_multi_head_attention.py
@@ -1,16 +1,30 @@
 import torch
 import math
 from ._weighting import Linear
+from ._functional import mask_chronological
 import torch.nn.functional as F
-from typing import Optional
+from typing import Optional, Tuple, Union
 
 
 class MultiHeadAttention(torch.nn.Module):
 
-    def __init__(self, projection_dim: int, n_heads: int):
+    def __init__(self, projection_dim: int, n_heads: int,
+                 linear_complexity: bool = True):
+        """
+        Parameters
+        ----------
+        projection_dim : int
+            the dimension of the projection space for the feature vectors
+        n_heads : int
+            the number of different projection at each stage of the transformer
+        linear_complexity : bool
+            if True, a variant of the attention mechanism is used that has
+            linear coplexity with the sequence length of tokens
+        """
         super().__init__()
         self.n_heads = n_heads
         self.projection_dim = projection_dim
+        self.linear_complexity = linear_complexity
         dim = projection_dim*n_heads
         self.query = Linear(dim, dim, bias=False)
         self.key = Linear(dim, dim, bias=False)
@@ -23,6 +37,7 @@ class MultiHeadAttention(torch.nn.Module):
         torch.nn.Module.__init__(obj)
         obj.n_heads = dump["n heads"]
         obj.projection_dim = dump["projection dim"]
+        obj.linear_complexity = dump["linear complexity"]
         obj.query = Linear.from_dump(dump["query"])
         obj.key = Linear.from_dump(dump["key"])
         obj.value = Linear.from_dump(dump["value"])
@@ -55,33 +70,114 @@ class MultiHeadAttention(torch.nn.Module):
         torch.Tensor :
             tensor of shape (N, Lq, D)
         """
-        return self._multihead_attention_stock(query, key, mask)
+        if self.linear_complexity:
+            return self._multihead_attention(query, key, (mask is not None))
+        else:
+            # pytorch's stock implementation is more memory efficient
+            return self._multihead_attention_stock(query, key, mask)
 
     def _multihead_attention(self, query: torch.Tensor, key: torch.Tensor,
-                             mask: Optional[torch.Tensor]):
+                             mask: Union[Optional[torch.Tensor], bool]
+                             ) -> torch.Tensor:
         """
         Apply multihead attention.
         Same inputs/outputs types/shapes as the forward pass
         """
         N, Lq, _ = query.shape
         N, Lk, _ = key.shape
+        # project into 'n_heads' different subspaces
         q = self.query(query).reshape(N, Lq, self.n_heads, self.projection_dim)
         k = self.key(key).reshape(N, Lk, self.n_heads, self.projection_dim)
         v = self.value(key).reshape(N, Lk, self.n_heads, self.projection_dim)
+        # compute attention dot product
         q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
-        attention, _ = self._scaled_dot_product_attention(q, k, v, mask)
+        if self.linear_complexity:
+            attention, _ = self._linear_complexity_attention(q, k, v, mask)
+        else:
+            attention, _ = self._scaled_dot_product_attention(q, k, v, mask)
         attention = attention.transpose(2, 1).reshape(N, Lq, -1)
         return attention
+
+    def _linear_complexity_attention(self, q: torch.Tensor, k: torch.Tensor,
+                                     v: torch.Tensor, mask: bool = False
+                                     ) -> Tuple[torch.Tensor, None]:
+        """
+        This is an alternative attention function.
+        'softmax(Q*Kt)*V' becomes 'softmax(Q)*softmax(K)*V'
+        This allow to chose operation order for the 3 matrices multiplications
+        and have linear (instead of squared) complexity with sequence length or
+        projection dim depending on the situation.
+
+
+        This implementation reproduces this paper :
+            'Efficient Attention: Attention with Linear Complexities'
+            https://arxiv.org/pdf/1812.01243.pdf
+        The masking for causal-attention is inspired from the paper :
+            'Rethinking Attention with Performers', Annexe B.1
+            https://arxiv.org/pdf/2009.14794.pdf
+
+        Parameters
+        ----------
+        q : torch.Tensor
+            query tensor of shape (N, H, Lq, D)
+        k : torch.Tensor
+            key tensor of shape (N, H, Lk, D)
+        v : torch.Tensor
+            value tensor of shape (N, H, Lk, D)
+        mask : bool
+            If True, returns unidirectional attention
+
+        Returns
+        -------
+        tuple of (torch.tensor, None) :
+            the attention score cannot always be calculated,
+            so None is returned instead
+        """
+        if not isinstance(mask, bool):
+            raise ValueError("Custom masking is not supported by linear "
+                             "complexity attention. 'mask' must be True for "
+                             "unidirectional, and False for bidirectional "
+                             "attention.")
+        _, _, Lq, D = q.shape
+        _, _, Lk, _ = k.shape
+        q = torch.softmax(q, dim=-1)
+        kt = torch.softmax(k, dim=-1).transpose(-2, -1)
+        # check the most costly operation order between (q*kt)*v and q*(kt*v)
+        left_first = Lq*Lk <= D**2
+        # perform operations
+        if mask:
+            if left_first:
+                left = torch.matmul(q, kt)
+                mask = mask_chronological(Lq, Lk, left.device)
+                left = left.masked_fill(mask, 0.)
+                result = torch.matmul(left, v)
+                return result, None
+            else:
+                right = torch.cumsum(
+                    torch.einsum("...ik, ...kj -> ...kij", kt, v), dim=-3)
+                result = torch.matmul(q.unsqueeze(-2), right).squeeze(-2)
+                return result, None
+        else:
+            if left_first:
+                left = torch.matmul(q, kt)
+                return torch.matmul(left, v), None
+            else:
+                right = torch.matmul(kt, v)
+                return torch.matmul(q, right), None
 
     def _scaled_dot_product_attention(self, q: torch.Tensor, k: torch.Tensor,
                                       v: torch.Tensor,
                                       mask: Optional[torch.Tensor]
-                                      ) -> torch.Tensor:
+                                      ) -> Tuple[torch.Tensor]:
         """
-        Apply scaled dot product attention to a batch of 'N' observations, with
-        'H' the number of heads, and 'D' the projection dimension.
+        Apply scaled dot product attention to a batch of 'N' sentences pairs,
+        with 'H' the number of heads, and 'D' the projection dimension.
         The query is a sequence of length 'Lq', and the key is
         a sequence of length 'Lk'.
+
+        This is the original attention mechanism described in the 2017 paper:
+            'Attention is all you need'
+            https://arxiv.org/pdf/1706.03762.pdf
 
         Parameters
         ----------
@@ -99,10 +195,10 @@ class MultiHeadAttention(torch.nn.Module):
         tuple of torch.Tensors:
             a tuple of (attention, score)
         """
-        Lk = k.shape[-1]
+        Lk = k.shape[-2]
         score = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(Lk)
         if mask is not None:
-            score = score.masked_fill(mask, -1.0E6)
+            score = score.masked_fill(mask, -1.0E300)
         score = torch.softmax(score, dim=-1)
         attention = torch.matmul(score, v)
         return attention, score
@@ -150,6 +246,7 @@ class MultiHeadAttention(torch.nn.Module):
         return {"type": type(self).__name__,
                 "n heads": self.n_heads,
                 "projection dim": self.projection_dim,
+                "linear complexity": self.linear_complexity,
                 "query": self.query.dump,
                 "key": self.key.dump,
                 "value": self.value.dump}
