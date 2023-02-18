162b26cec233a528c6f5977b8575713d21518948
BenoitFavier <bade@webmails.top>
2023-02-18 06:07:34 +0100

    modified variable names

--- pygmalion/neural_networks/_dense_regressor.py
+++ pygmalion/neural_networks/_dense_regressor.py
@@ -12,17 +12,25 @@ from .layers import Activation
 
 class DenseRegressor(NeuralNetwork):
 
-    def __init__(self, input_names: Iterable[str],
-                 target_names: Union[str, Iterable[str]],
+    def __init__(self, inputs: Iterable[str],
+                 target: Union[str, Iterable[str]],
                  hidden_layers: Iterable[int],
                  activation: str = "relu",
                  batch_norm: bool = True,
                  dropout: Optional[float] = None):
+        """
+        Parameters
+        ----------
+        inputs : Iterable of str
+            the column names of the input variables in a dataframe
+        target : str or Iterable of str
+
+        """
         super().__init__()
-        self.input_names = tuple(input_names)
-        self.target_names = target_names
+        self.inputs = tuple(inputs)
+        self.target = target if isinstance(target, str) else tuple(target)
         self.layers = torch.nn.ModuleList()
-        in_features = len(input_names)
+        in_features = len(inputs)
         if batch_norm:
             self.layers.append(torch.nn.BatchNorm1d(in_features))
         for out_features in hidden_layers:
@@ -33,7 +41,7 @@ class DenseRegressor(NeuralNetwork):
             if dropout is not None:
                 self.layers.append(torch.nn.Dropout(dropout))
             in_features = out_features
-        out_features = 1 if isinstance(target_names, str) else len(self.target_names)
+        out_features = 1 if isinstance(target, str) else len(self.target)
         self.output = torch.nn.Linear(in_features, out_features)
         if batch_norm:
             self.target_norm = torch.nn.BatchNorm1d(out_features, affine=False)
@@ -65,20 +73,20 @@ class DenseRegressor(NeuralNetwork):
 
     def _x_to_tensor(self, x: Union[pd.DataFrame, dict, Iterable],
                      device: Optional[torch.device] = None):
-        return named_to_tensor(x, list(self.input_names), device=device)
+        return named_to_tensor(x, list(self.inputs), device=device)
 
     def _y_to_tensor(self, y: Union[pd.DataFrame, dict, Iterable],
                      device: Optional[torch.device] = None) -> torch.Tensor:
-        if isinstance(self.target_names, str):
-            return floats_to_tensor(y[self.target_names], device=device).unsqueeze(-1)
+        if isinstance(self.target, str):
+            return floats_to_tensor(y[self.target], device=device).unsqueeze(-1)
         else:
-            return named_to_tensor(y, list(self.target_names), device=device)
+            return named_to_tensor(y, list(self.target), device=device)
 
     def _tensor_to_y(self, tensor: torch.Tensor) -> np.ndarray:
         if self.target_norm is not None:
             tensor = (tensor * (self.target_norm.running_var + self.target_norm.eps)**0.5
                       + self.target_norm.running_mean)
-        if isinstance(self.target_names, str):
+        if isinstance(self.target, str):
             return tensor_to_floats(tensor).reshape(-1)
         else:
-            return tensor_to_dataframe(tensor, self.target_names)
+            return tensor_to_dataframe(tensor, self.target)
