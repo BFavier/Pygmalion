corrected a bug in normalization of kernelized attention

pygmalion/neural_networks/layers/transformers/_functions.py
@@ -84,16 +84,16 @@ def _kernelized_attention_naive(kernel: Callable, q: torch.Tensor, k: torch.Tens
     torch.Tensor:
         attention, a tensor of shape (N, H, Lq, D)
     """
-    q, k = kernel(q), kernel(k)
-    N, H, Lq, d = q.shape
-    N, H, Lk, d = k.shape
-    score = torch.einsum("nhqd, nhkd -> nhqk", q, k)
+    pq, pk = kernel(q), kernel(k)
+    N, H, Lq, d = pq.shape
+    N, H, Lk, d = pk.shape
+    score = torch.einsum("nhqd, nhkd -> nhqk", pq, pk)
     if RPE is not None:
         r = RPE.weight.shape[0] // 2
         P = torch.clip(r + torch.arange(Lk, device=score.device).reshape(1, Lk)
                        - torch.arange(Lq, device=score.device).reshape(Lq, 1), 0, 2*r)
         P = RPE(P)
-        score = score + torch.einsum("qkd, nhkd -> nhqk", P, k)
+        score = score + torch.einsum("qkd, nhkd -> nhqk", P, pk)
     if mask is not None:
         score = score.masked_fill(mask, 0)
     if padding_mask is not None:
@@ -145,25 +145,25 @@ def _kernelized_attention_linear(kernel: Callable, q: torch.Tensor, k: torch.Ten
     torch.Tensor:
         attention, a tensor of shape (N, H, Lq, D)
     """
-    q, k = kernel(q), kernel(k)
+    pq, pk = kernel(q), kernel(k)
     if RPE is not None:
         RPE = kernel(RPE.weight)
     if padding_mask is not None:
         raise NotImplementedError()
-    N, H, Lq, D = q.shape
-    N, H, Lk, D = k.shape
+    N, H, Lq, D = pq.shape
+    N, H, Lk, D = pk.shape
     if mask:
-        expanded = torch.einsum("nhkD, nhkd -> nhkdD", k, v)
+        expanded = torch.einsum("nhkD, nhkd -> nhkdD", pk, v)
         summed = _align(torch.cumsum(expanded, dim=2), Lq, 2)
-        attention = torch.einsum("nhqd, nhqdD -> nhqD", q, summed)
+        attention = torch.einsum("nhqd, nhqdD -> nhqD", pq, summed)
     else:
-        right = torch.einsum("nhkd, nhkD -> nhdD", k, v)
-        attention = torch.einsum("nhqd, nhdD -> nhqD", q, right)
+        right = torch.einsum("nhkd, nhkD -> nhdD", pk, v)
+        attention = torch.einsum("nhqd, nhdD -> nhqD", pq, right)
     if RPE is not None:
         r = RPE.shape[0] // 2
         if mask:
             RPE = torch.masked_fill(RPE, torch.arange(2*r+1).unsqueeze(-1) > r, 0.)
-        W = torch.einsum("nhqd, Rd -> nhqR", q, RPE)
+        W = torch.einsum("nhqd, Rd -> nhqR", pq, RPE)
         # before horizon
         p_before = max(r, Lq)
         W_before = W[..., 0]  # (N, H, Lq)
@@ -173,10 +173,10 @@ def _kernelized_attention_linear(kernel: Callable, q: torch.Tensor, k: torch.Ten
         # horizon
         W_horizon = W[..., 1:-1]  # (N, H, Lq, 2r-1)
         V_horizon = torch.cat([torch.zeros((N, H, max(0, r-1), D),
-                                           device=q.device),
+                                           device=pq.device),
                                v,
                                torch.zeros((N, H, max(0, Lq-(Lk-r)), D),
-                                           device=q.device)],
+                                           device=pq.device)],
                                dim=-2)
         L = V_horizon.shape[-2]
         V_horizon = V_horizon.as_strided(size=(N, H, Lq, 2*r-1, D),
@@ -187,7 +187,7 @@ def _kernelized_attention_linear(kernel: Callable, q: torch.Tensor, k: torch.Ten
             n_after = min(Lq+r, Lk)
             p_after = max(0, Lq-max(0, Lk-r))
             W_after = W[..., -1]  # (N, H, Lq)
-            padding_after = torch.zeros((N, H, p_after, D), device=q.device)
+            padding_after = torch.zeros((N, H, p_after, D), device=pq.device)
             Rcum = (v[..., r-1:n_after, :].sum(dim=-2).unsqueeze(-2)
                     - v[..., r-1:n_after-1, :].cumsum(dim=-2))
             V_after = torch.cat([Rcum, padding_after], dim=-2)