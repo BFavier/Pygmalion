52e3180251ef5371de90ee637ffef2035ef4abf4
Benoit Favier <benoitfamillefavier@gmail.com>
2023-03-10 18:52:17 +0100

    draft text translator

--- pygmalion/neural_networks/_text_translator.py
+++ pygmalion/neural_networks/_text_translator.py
@@ -1,58 +1,45 @@
 import torch
+import numpy as np
 from typing import Union, List, Dict, Tuple, Optional, Callable
-from .layers import Transformer, Embedding
-from .layers import Linear, Dropout
-from .layers import mask_chronological
+from .layers.transformers import TransformerEncoder, TransformerDecoder, ATTENTION_TYPE
 from ._conversions import sentences_to_tensor, tensor_to_sentences
 from ._conversions import floats_to_tensor
 from ._neural_network import NeuralNetwork
 from ._loss_functions import cross_entropy
-from .layers._functional import positional_encoding
-from pygmalion.unsupervised import tokenizers
-from pygmalion.tokenizers import Tokenizer, SpecialToken
-from pygmalion.utilities import document
+from pygmalion.tokenizers._utilities import Tokenizer, SpecialToken
 
 
 
 
 class TextTranslator(NeuralNetwork):
 
-    def __init__(self,
-                 tokenizer_in: Tokenizer,
-                 tokenizer_out: Tokenizer,
-                 n_stages: int,
-                 projection_dim: int,
-                 n_heads: int,
-                 max_length: Optional[int] = None,
+    def __init__(self, tokenizer_input: Tokenizer,
+                 tokenizer_output: Tokenizer,
+                 n_stages: int, projection_dim: int, n_heads: int,
                  activation: str = "relu",
-                 dropout: Union[float, None] = None):
+                 dropout: Union[float, None] = None,
+                 mask_padding: bool = False,
+                 attention_type: ATTENTION_TYPE = "scaled dot product",
+                 RPE_radius: Optional[int] = None):
         """
         Parameters
         ----------
         ...
-        max_length : int
-            the maximum length of token sequence
-            sequences bigger are dropped
-        activation : str
-            the default value for the 'activation' key of the kwargs
-        dropout : float or None
-            the default value for the 'dropout' key of the kwargs
         """
         super().__init__()
-        self.max_length = max_length
         embedding_dim = projection_dim*n_heads
-        self.tokenizer_in = tokenizer_in
-        self.tokenizer_out = tokenizer_out
-        self.embedding_in = Embedding(self.tokenizer_in.n_tokens+3,
-                                      embedding_dim)
-        self.embedding_out = Embedding(self.tokenizer_out.n_tokens+3,
-                                       embedding_dim)
-        self.dropout_in = Dropout(dropout)
-        self.dropout_out = Dropout(dropout)
-        self.transformer = Transformer(n_stages, projection_dim, n_heads,
-                                       dropout=dropout, activation=activation)
-        self.output = Linear(embedding_dim,
-                             self.tokenizer_out.n_tokens+3)
+        self.tokenizer_input = tokenizer_input
+        self.tokenizer_output = tokenizer_output
+        self.embedding_in = torch.nn.Embedding(self.tokenizer_input.n_tokens,
+                                               embedding_dim)
+        self.dropout_in = torch.nn.Dropout(dropout) if dropout is not None else None
+        self.transformer_encoder = TransformerEncoder(...)
+        self.embedding_out = torch.nn.Embedding(self.tokenizer_output.n_tokens,
+                                                embedding_dim)
+        self.dropout_out = torch.nn.Dropout(dropout) if dropout is not None else None
+        self.transformer_decoder = TransformerDecoder(n_stages, projection_dim, n_heads,
+                                                      dropout=dropout, activation=activation)
+        self.output = torch.nn.Linear(embedding_dim, self.tokenizer_output.n_tokens)
 
     def forward(self, X):
         return self.encode(X)
@@ -140,17 +127,25 @@ class TextTranslator(NeuralNetwork):
                         weights: None = None,
                         device: torch.device = torch.device("cpu")) -> tuple:
         if X is not None:
-            x = sentences_to_tensor(X, self.module.tokenizer_in, device,
-                                    max_sequence_length=self.module.max_length)
+            x = 
         else:
             x = None
         if Y is not None:
-            y = sentences_to_tensor(Y, self.module.tokenizer_out, device,
-                                    max_sequence_length=self.module.max_length)
+            y = 
         else:
             y = None
         w = None if weights is None else floats_to_tensor(weights, device)
         return x, y, w
 
-    def _tensor_to_y(self, tensor: torch.Tensor) -> List[str]:
-        return tensor_to_sentences(tensor, self.module.tokenizer_out)
+    def _x_to_tensor(self, x: List[str],
+                     device: Optional[torch.device] = None):
+        return sentences_to_tensor(x, self.tokenizer_input, device,
+                                    max_sequence_length=self.module.max_length)
+
+    def _y_to_tensor(self, y: List[str],
+                     device: Optional[torch.device] = None) -> torch.Tensor:
+        return sentences_to_tensor(y, self.tokenizer_output, device,
+                                   max_sequence_length=self.module.max_length)
+
+    def _tensor_to_y(self, tensor: torch.Tensor) -> np.ndarray:
+        return tensor_to_sentences(tensor, self.tokenizer_output)
