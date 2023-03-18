9f3ab156c2a9bb770554c3c7bde76dc09af1966f
Benoit Favier <benoitfamillefavier@gmail.com>
2023-03-18 21:38:48 +0100

    modified the sentencepairs training script

--- samples/models/advanced/sentencepairs.py
+++ samples/models/advanced/sentencepairs.py
@@ -1,41 +1,54 @@
+from typing import Iterable
 import pygmalion as ml
 import matplotlib.pyplot as plt
 import pandas as pd
 import pathlib
 import IPython
+import torch
 
-path = pathlib.Path(__file__).parent
+path = pathlib.Path(__file__).parents[2]
 data_path = path / "data"
 
 # Download the data
-ml.datasets.sentence_pairs(data_path)
+# ml.datasets.sentence_pairs(data_path)
 
-df = pd.read_csv(data_path / "sentence_pairs.txt", header=None,
-                 names=["en", "fr"], sep="\t")
-en, fr = df["en"].str.lower(), df["fr"].str.lower()
+df = pd.read_csv(data_path / "QED_v2.0a.csv.gz")
+df_train = df.sample(frac=0.8)
+df_val = df.drop(index=df_train.index)
 
-tokenizer_in = ml.unsupervised.tokenizers.WhitespaceTokenizer()
-c1 = tokenizer_in.train(fr)
-tokenizer_out = ml.unsupervised.tokenizers.WhitespaceTokenizer()
-c2 = tokenizer_out.train(en)
+class Looper:
 
+    def __init__(self, series: Iterable[str], batch_size: int):
+        self.series = pd.Series(series)
+        self.batch_size = batch_size
+    
+    def __iter__(self):
+        while True:
+            yield self.series.sample(n=self.batch_size)
 
-n_stages = 4
-projection_dim = 128
-n_heads = 4
-model = ml.neural_networks.Traductor(tokenizer_in, tokenizer_out,
-                                     n_stages, projection_dim, n_heads,
-                                     GPU=0, optimization_method="Adam")
+tokenizer_in = ml.tokenizers.BytePairEncoder()
+tokenizer_in.fit(Looper(df.fr, 1000), max_vocabulary_size=20000)
+tokenizer_out = ml.tokenizers.BytePairEncoder()
+tokenizer_out.fit(Looper(df.en, 1000), max_vocabulary_size=20000)
 
-model.train((fr, en), n_epochs=10000, learning_rate=1.0E-3,
-            batch_size=20, n_batches=10, keep_best=False)
+model = ml.neural_networks.TextTranslator(tokenizer_in, tokenizer_out, n_stages=6, projection_dim=16, n_heads=8,
+                                          positional_encoding_type=None, RPE_radius=16, dropout=0.1)
+model.to("cuda:0")
 
-model.plot_history()
+class Batchifyer:
+
+    def __init__(self, df: pd.DataFrame, model: ml.neural_networks.TextTranslator, batch_size: int):
+        self.df = df
+        self.batch_size = batch_size
+    
+    def __iter__(self):
+        sub = df.sample(n=self.batch_size)
+        yield model.data_to_tensor(sub.fr, sub.en)
+
+train = Batchifyer(df_train, model, batch_size=100)
+val = Batchifyer(df_val, model, batch_size=100)
+
+train_losses, val_losses, best_step = model.fit(train, val, n_steps=3000, learning_rate=1.0E-3)
+ml.plot_losses(train_losses, val_losses, best_step)
 plt.show()
-
-for sentence in fr[:3]:
-    print()
-    print(sentence)
-    print(model(sentence, max_words=50))
-
 IPython.embed()
