c559c7023c93a79b01299eaaf816fd83ffa4e918
Benoit Favier <benoitfamillefavier@gmail.com>
2023-03-19 17:28:40 +0100

    changed sentence_pairs dataset

--- pygmalion/datasets/_sentence_pairs.py
+++ pygmalion/datasets/_sentence_pairs.py
@@ -3,9 +3,8 @@ from ._download import download
 
 def sentence_pairs(directory: str):
     """
-    downloads the modified en-fr 'Bilingual sentence pairs' dataset
+    downloads the modified en-fr 'TildeMODEL v2018' dataset from OPUS
     in the given directory
     """
-    download(directory, "sentence_pairs.txt",
-             "https://drive.google.com/file/d"
-             "/12IiCD6Gm4kFHSO2GtXPipgsjdW89e338/view?usp=sharing")
+    download(directory, "sentence_pairs.csv.gz",
+             "https://drive.google.com/file/d/1RmBm7qwTn-UlSsaWM-6K8eXk8QnZVC5x/view?usp=share_link")
