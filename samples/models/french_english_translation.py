0fff89c20a195da725c2226ecd3f2080f7cea1a6
Benoit Favier <benoitfamillefavier@gmail.com>
2023-04-07 11:46:32 +0200

    modified training script

--- samples/models/french_english_translation.py
+++ samples/models/french_english_translation.py
@@ -27,20 +27,12 @@ class Looper:
         while True:
             yield self.series.sample(n=self.batch_size)
 
-tok_in_file = path / "tokenizer_in.json"
-if tok_in_file.is_file():
-    tokenizer_in = ml.tokenizers.BytePairEncoder.load(tok_in_file)
-else:
-    tokenizer_in = ml.tokenizers.BytePairEncoder()
-    tokenizer_in.fit(Looper(df.fr, 1000), max_vocabulary_size=10000)
-    tokenizer_in.save(tok_in_file)
-tok_out_file = path / "tokenizer_out.json"
-if tok_out_file.is_file():
-    tokenizer_out = ml.tokenizers.BytePairEncoder.load(tok_out_file)
-else:
-    tokenizer_out = ml.tokenizers.BytePairEncoder()
-    tokenizer_out.fit(Looper(df.en, 1000), max_vocabulary_size=10000)
-    tokenizer_out.save(tok_out_file)
+
+tokenizer_in = ml.tokenizers.BytePairEncoder()
+tokenizer_in.fit(Looper(df.fr, 1000), max_vocabulary_size=10000)
+
+tokenizer_out = ml.tokenizers.BytePairEncoder()
+tokenizer_out.fit(Looper(df.en, 1000), max_vocabulary_size=10000)
 
 model = ml.neural_networks.TextTranslator(tokenizer_in, tokenizer_out, n_stages=6, projection_dim=64, n_heads=8,
                                           RPE_radius=8, dropout=0.1,
@@ -55,8 +47,6 @@ class Batchifyer:
                                               max_input_sequence_length=sequence_length,
                                               max_output_sequence_length=sequence_length,
                                               progress_bar=True)
-        # self.df = df
-        # self.model = model
         self.batch_size = batch_size
         self.n_batches = n_batches
 
@@ -65,18 +55,11 @@ class Batchifyer:
             indexes = torch.randperm(len(self.x))[:self.batch_size]
             yield (self.x[indexes], self.y[indexes])
 
-    # def __iter__(self):
-    #     for _ in range(self.n_batches):
-    #         sub = self.df.sample(n=self.batch_size)
-    #         yield self.model.data_to_tensor(sub.fr, sub.en,
-    #                                         max_input_sequence_length=128,
-    #                                         max_output_sequence_length=128)
-
 train = Batchifyer(df_train, model, batch_size=800, n_batches=1)
 val = Batchifyer(df_val, model, batch_size=800, n_batches=1)
 optimizer = torch.optim.Adam(model.parameters(), lr=0., betas=(0.9, 0.98))
 train_losses, val_losses, grad, best_step = model.fit(train, val, optimizer,
-    n_steps=100000, patience=None, keep_best=True,
+    n_steps=20000, patience=None, keep_best=True,
     learning_rate=lambda step: 512**-0.5 * min((step+1)**-0.5, (step+1) * 4000**-1.5))
 
 ml.plot_losses(train_losses, val_losses, grad, best_step);plt.show()
