0248d67fbcf7fdb91c869200ad1d56fe10b6db63
Benoit Favier <benoitfamillefavier@gmail.com>
2023-04-14 16:28:39 +0200

    import PaddedConv as well

--- pygmalion/neural_networks/layers/__init__.py
+++ pygmalion/neural_networks/layers/__init__.py
@@ -1,11 +1,6 @@
 from ._activation import Activation
 from ._conv_block import ConvBlock
+from ._padded_conv import PaddedConv2d
 from ._upsampling import Upsampling1d, Upsampling2d, UPSAMPLING_METHOD
 from ._normalizer import Normalizer
 from ._positional_encoding import LearnedPositionalEncoding, SinusoidalPositionalEncoding
-# from ._transformer import TransformerEncoderStage, TransformerDecoderStage
-# from ._transformer import TransformerEncoder, TransformerDecoder
-# from ._transformer import Transformer
-# from ._multi_head_attention import MultiHeadAttention
-# from ._u_net import UNet1d, UNet2d
-# from ._functional import mask_chronological
