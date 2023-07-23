dc1c766e5e3bfcea643d2a2a6c75b47735124857
Benoit Favier <benoitfamillefavier@gmail.com>
2023-07-23 12:49:04 +0200

    makes multihead_attention types accessible from pygmalion.neural_networks.layers

--- pygmalion/neural_networks/layers/__init__.py
+++ pygmalion/neural_networks/layers/__init__.py
@@ -2,3 +2,4 @@ from ._activation import Activation
 from ._dropout import Dropout, Dropout1d, Dropout2d
 from ._normalization import Normalizer, FeaturesNorm
 from ._positional_encoding import LearnedPositionalEncoding, SinusoidalPositionalEncoding
+from .transformers.multihead_attention import *
