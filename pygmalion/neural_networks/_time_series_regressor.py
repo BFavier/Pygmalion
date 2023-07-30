3131d121650f9d549d85c463ec1d24be9cf683d9
Benoit Favier <benoitfamillefavier@gmail.com>
2023-07-30 22:02:55 +0200

    draft time series regressor

--- pygmalion/neural_networks/_time_series_regressor.py
+++ pygmalion/neural_networks/_time_series_regressor.py
@@ -1,19 +1,18 @@
 import torch
 import pandas as pd
-from typing import Union, List, Optional, Iterable
+from typing import Union, Optional, Iterable
 from .layers.transformers import TransformerEncoder, ATTENTION_TYPE, FourrierKernelAttention
 from .layers.positional_encoding import POSITIONAL_ENCODING_TYPE
 from .layers import Dropout, Normalizer
 from ._conversions import named_to_tensor, tensor_to_dataframe
-from ._conversions import classes_to_tensor
 from ._neural_network import NeuralNetwork
 from ._loss_functions import MSE
-from pygmalion.tokenizers._utilities import Tokenizer
 
 
 class TimeSeriesRegressor(NeuralNetwork):
 
     def __init__(self, inputs: Iterable[str], targets: Iterable[str],
+                 observation_column: str, time_column: Optional[str],
                  n_stages: int, projection_dim: int, n_heads: int,
                  activation: str = "relu",
                  dropout: Union[float, None] = None,
@@ -55,11 +54,11 @@ class TimeSeriesRegressor(NeuralNetwork):
         super().__init__()
         self.inputs = list(inputs)
         self.targets = list(targets)
+        self.observation_column = observation_column
+        self.time_column = str(time_column) if time_column is not None else None
         embedding_dim = projection_dim*n_heads
-        self.inputs = inputs
-        self.targets = targets
-        self.input_normalizer = Normalizer(-1, len(inputs), affine=False) if normalize else None
-        self.target_normalizer = Normalizer(-1, len(targets), affine=False) if normalize else None
+        self.input_normalizer = Normalizer(-1, len(inputs)) if normalize else None
+        self.target_normalizer = Normalizer(-1, len(targets)) if normalize else None
         self.initial_embedding = torch.nn.parameter.Parameter(torch.zeros(1, 1, embedding_dim))
         self.embedding = torch.nn.Linear(len(inputs), embedding_dim)
         self.dropout_input = Dropout(dropout)
@@ -74,7 +73,7 @@ class TimeSeriesRegressor(NeuralNetwork):
                                                       **attention_kwargs)
         self.head = torch.nn.Linear(embedding_dim, len(self.targets))
 
-    def forward(self, X: torch.Tensor, T: torch.Tensor, padding_mask: Optional[torch.Tensor], initial_embedding: bool=False):
+    def forward(self, X: torch.Tensor, T: Optional[torch.Tensor], padding_mask: Optional[torch.Tensor], initial_embedding: bool=False):
         """
         performs the encoding part of the network
 
@@ -82,7 +81,7 @@ class TimeSeriesRegressor(NeuralNetwork):
         ----------
         X : torch.Tensor
             tensor of floats of shape (N, L, D)
-        T : torch.Tensor
+        T : torch.Tensor or None
             tensor of floats of shape (N, L)
         padding_mask : torch.Tensor or None
             tensor of booleans of shape (N, L)
@@ -93,32 +92,38 @@ class TimeSeriesRegressor(NeuralNetwork):
             tensor of floats of shape (N, L, D)
         """
         X = X.to(self.device)
-        T = T.to(self.device)
-        N, L, _ = X.shape
+        if T is not None:
+            T = T.to(self.device)
         if self.input_normalizer is not None:
             X = self.input_normalizer(X)
         X = self.embedding(X)
         if initial_embedding:
-            X = torch.concatenate([self.initial_embedding, X], dim=1)
+            X = torch.concatenate([self.initial_embedding.expand(len(X), -1, -1), X], dim=1)
+        N, L, _ = X.shape
         if self.positional_encoding is not None:
             X = self.positional_encoding(X)
         X = self.dropout_input(X.reshape(N*L, -1)).reshape(N, L, -1)
-        X = self.transformer_encoder(X, padding_mask, attention_kwargs={"query_positions": T, "key_postions": T})
+        attention_kwargs = {"query_positions": T, "key_positions": T} if T is not None else {}
+        X = self.transformer_encoder(X, padding_mask, attention_kwargs=attention_kwargs)
         return self.head(X)
 
-    def loss(self, x, t, padding_mask, y_target, weights=None):
+    def loss(self, x: torch.Tensor, t: Optional[torch.Tensor], padding_mask: torch.Tensor,
+             y_target: torch.Tensor, weights: Optional[torch.Tensor]=None):
         """
         Parameters
         ----------
         x : torch.Tensor
             tensor of floats of shape (N, L, D)
-        t : torch.Tensor
+        t : torch.Tensor or None
+            if provided, the time as a tensor of floats of shape (N, L)
         padding_mask : torch.Tensor
             tensor of booleans of shape (N, L)
         y_target : torch.Tensor
-            tensor of floats of shape (N, L, D )
+            tensor of floats of shape (N, L, D)
         """
-        x, t, y_target = x.to(self.device), t.to(self.device), y_target.to(self.device)
+        x, y_target = x.to(self.device), y_target.to(self.device)
+        if t is not None:
+            t = t.to(self.device)
         y_pred = self(x[:, :-1, :], t, padding_mask, initial_embedding=True)
         if self.target_normalizer is not None:
             y_target = self.target_normalizer(y_target)
@@ -128,13 +133,51 @@ class TimeSeriesRegressor(NeuralNetwork):
     def device(self) -> torch.device:
         return self.head.weight.device
 
-    def _x_to_tensor(self, x: Union[pd.DataFrame, dict, Iterable],
-                     device: Optional[torch.device] = None):
-        return named_to_tensor(x, list(self.inputs), device=device)
+    def data_to_tensor(self, df: pd.DataFrame,
+                       device: Optional[torch.device] = None,
+                       padded_sequence_length: Optional[int] = None,
+                       raise_on_longer_sequences: bool = False) -> tuple:
+        X, T, padding_mask = self._x_to_tensor(df, device, padded_sequence_length, raise_on_longer_sequences)
+        Y = self._y_to_tensor(df, device, padded_sequence_length)
+        return X, T, padding_mask, Y
 
-    def _y_to_tensor(self, y: Union[pd.DataFrame, dict, Iterable],
-                     device: Optional[torch.device] = None) -> torch.Tensor:
-        return named_to_tensor(y, self.targets, device=device)
+    def _x_to_tensor(self, df: pd.DataFrame, device: Optional[torch.device] = None,
+                     padded_sequence_length: Optional[int] = None,
+                     raise_on_longer_sequences: bool = False):
+        if raise_on_longer_sequences and padded_sequence_length is not None:
+            for obs, x in df.groupby(self.observation_column):
+                if len(x) > padded_sequence_length:
+                    raise RuntimeError(f"Found sequence longer than {padded_sequence_length} for observation '{obs}'")
+        Xs = [named_to_tensor(x, self.inputs) for _, x in df.groupby(self.observation_column)]
+        if padded_sequence_length is None:
+            padded_sequence_length = max(len(x) for x in Xs)
+        X = torch.stack([torch.cat([x, torch.full([padded_sequence_length-len(x), len(self.targets)], float("nan"))])
+                         for x in Xs if len(x) <= padded_sequence_length], dim=0)
+        padding_mask = torch.stack([(torch.arange(padded_sequence_length) < len(x))
+                                    for x in Xs if len(x) <= padded_sequence_length], dim=0)
+        if self.time_column is not None:
+            Ts = [named_to_tensor(x, [self.time_column])
+                for _, x in df.groupby(self.observation_column)]
+            T = torch.stack([torch.cat([t, torch.full([padded_sequence_length-len(t), 1], float("nan"))])
+                             for t in Ts if len(t) <= padded_sequence_length], dim=0)
+        else:
+            T = None
+        if device is not None:
+            X = X.to(device)
+            T = T.to(device)
+            padding_mask = padding_mask.to(device)
+        return X, T, padding_mask
+
+    def _y_to_tensor(self, df: pd.DataFrame, device: Optional[torch.device] = None,
+                     padded_sequence_length: Optional[int] = None) -> torch.Tensor:
+        Ys = [named_to_tensor(y, self.targets) for _, y in df.groupby(self.observation_column)]
+        if padded_sequence_length is None:
+            padded_sequence_length = max(len(y) for y in Ys)
+        Y = torch.stack([torch.cat([y, torch.full([padded_sequence_length-len(y), len(self.targets)], float("nan"))])
+                         for y in Ys if len(y) <= padded_sequence_length], dim=0)
+        if device is not None:
+            Y = Y.to(device)
+        return Y
 
     def _tensor_to_y(self, tensor: torch.Tensor) -> pd.DataFrame:
         return tensor_to_dataframe(tensor, self.targets)
--- samples/models/TimeSeriesRegressor.py
+++ samples/models/TimeSeriesRegressor.py
@@ -1,38 +1,29 @@
 from pygmalion.datasets.generators import OrbitalTrajectoryGenerator
-import matplotlib.pyplot as plt
-import pandas as pd
-from matplotlib.animation import FuncAnimation
-import time
+from pygmalion.neural_networks import TimeSeriesRegressor
+from pygmalion.neural_networks.layers.transformers import FourrierKernelAttention
 
-N_OBJECTS = 100
-t0 = time.perf_counter()
-df = OrbitalTrajectoryGenerator.generate_batch(batch_size=N_OBJECTS, dt_min=0., tol=1.0E-6)
-t1 = time.perf_counter()
-print(f"Execution time: {t1-t0:.3g} seconds")
-
-def plot_trajectory(df: pd.DataFrame):
-    f, ax = plt.subplots()
-    ax.set_xlim([-3, 3])
-    ax.set_ylim([-3, 3])
-    ax.set_aspect("equal")
-    lines = []
-
-    def update(df: pd.DataFrame, lines: list, frame: int):
-        for line, (_, sub) in zip(lines, df.groupby("obj")):
-            sub = sub.iloc[:frame]
-            line.set_data(sub["x"].tolist()[-20:], sub["y"].tolist()[-20:])
-        return lines
-
-    def init(lines: list, ax: plt.Axes):
-        lines.clear()
-        for _ in range(N_OBJECTS):
-            lines.append(ax.plot([], [])[0])
-
-    anim = FuncAnimation(f, lambda x: update(df, lines, x), frames=range(1, len(df)+1), init_func=lambda : init(lines, ax), blit=False, interval=5/len(df)*1.0E3)
-    plt.show()
+model = TimeSeriesRegressor(inputs=["x", "y", "u", "v"],
+                            targets=["x", "y", "u", "v"],
+                            observation_column="obj", time_column="t",
+                            n_stages=4, projection_dim=16, n_heads=4,
+                            attention_type=FourrierKernelAttention,
+                            attention_kwargs={"linear_complexity": False})
+model.to("cuda:0")
 
 
-plot_trajectory(df)
+class Batchifyer:
+
+    def __init__(self, n_batches: int, batch_size: int):
+        self.data_generator = OrbitalTrajectoryGenerator(n_batches=n_batches, batch_size=batch_size, dt_min=0., tol=1.0E-6)
+
+    def __iter__(self):
+        for batch in self.data_generator:
+            yield model.data_to_tensor(batch)
+
+
+train_data = Batchifyer(1, 10)
+model.fit(train_data)
+
 if __name__ == "__main__":
     import IPython
     IPython.embed()
