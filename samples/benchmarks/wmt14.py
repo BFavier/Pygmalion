56ee7e50a060299a5b2b2c612b0100d24033ad32
Benoit Favier <benoitfamillefavier@gmail.com>
2024-02-25 17:04:14 +0100

    added loading from checkpoint

--- samples/benchmarks/wmt14.py
+++ samples/benchmarks/wmt14.py
@@ -15,7 +15,13 @@ method = "vanilla"  # "new" or "vanilla" or "vanilla32"
 dataset = load_dataset('wmt14', 'fr-en', trust_remote_code=True)
 path = pathlib.Path(__file__).parent
 tokenizer_path = path / "tokenizer.json"
-if not tokenizer_path.is_file():
+model_checkpoint_path = path / "checkpoints" / "vanilla_model_050000.pth"
+optimizer_checkpoint_path = path / "checkpoints" / "optimizer_vanilla_model_050000.pth"
+
+if tokenizer_path.is_file():
+    print("Loading tokenizer")
+    tokenizer = load_model(tokenizer_path)
+else:
     tokenizer = BytePairEncoder()
 
     
@@ -31,19 +37,20 @@ if not tokenizer_path.is_file():
     tokenizer.fit(StringsBatchifyer(dataset["train"]),
                   max_vocabulary_size=20_000, min_frequency=0, pre_tokenize=True)
     tokenizer.save(tokenizer_path)
-else:
-    tokenizer = load_model(tokenizer_path)
 
-if method.startswith("vanilla"):
+if model_checkpoint_path.is_file():
+    print("Loading model")
+    model = torch.load(model_checkpoint_path)
+elif method.startswith("vanilla"):
     model = TextTranslator(tokenizer, tokenizer, n_stages=6,
                            projection_dim=16 if method.endswith("32") else 64,
                            n_heads=32 if method.endswith("32") else 8,
-                           dropout=None,
+                           dropout=0.1,
                            positional_encoding_type=SinusoidalPositionalEncoding,
                            attention_type=ScaledDotProductAttention)
 else:
     model = TextTranslator(tokenizer, tokenizer,
-                           n_stages=6, projection_dim=16, n_heads=32, dropout=None,
+                           n_stages=6, projection_dim=16, n_heads=32, dropout=0.1,
                            positional_encoding_type=None,
                            attention_type=FourrierKernelAttention)
 print(method, f"{sum(p.numel() for p in model.parameters()):.3g} parameters")
@@ -66,13 +73,14 @@ class Batchifyer:
             yield model.data_to_tensor(en, fr, max_input_sequence_length=self.padded_size,
                                        max_output_sequence_length=self.padded_size)
 
-
 optimizer = torch.optim.Adam(model.parameters(), betas=(0.9, 0.98))
+if optimizer_checkpoint_path.is_file():
+    optimizer.load_state_dict(torch.load(optimizer_checkpoint_path, map_location=model.device).state_dict())
 hist = model.fit(Batchifyer(dataset["train"], batch_size=100, n_batches=1),
                  Batchifyer(dataset["validation"], batch_size=100, n_batches=1),
-                 optimizer, n_steps=400_000, keep_best=False,
-                 learning_rate=lambda step: 1.0E-4 * 10**-(step/100_000),
-                 backup_path=path / "checkpoints", backup_prefix=f"{method}_model", backup_frequency=25_000)
+                 optimizer, n_steps=300_000, keep_best=False,
+                 learning_rate=lambda step: 1.0E-4 * 10**-(step/300_000),
+                 backup_path=path / "checkpoints", backup_prefix=f"{method}_model", backup_frequency=50_000)
 model.save(path / f"model_{method}.pth", overwrite=True)
 with open(path / f"history_{method}.json", "w", encoding="utf-8") as f:
     json.dump({"train_loss": hist[0], "val_loss": hist[1], "grad": hist[2], "best_step": hist[3]}, f)
